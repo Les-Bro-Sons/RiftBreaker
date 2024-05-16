748c92835b7de0ea9bfa6ac43cc2fe675bf92076
Lawice <129510417+Lawice@users.noreply.github.com>
2024-05-16 16:04:15 +0200

    Player Health + Boss Health + HourGlass

--- Assets/Scripts/Player/RB_Health.cs
+++ Assets/Scripts/Player/RB_Health.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class RB_Health : MonoBehaviour {
-    private float _hp;
+    [SerializeField] float _hp;
     [SerializeField] float _hpMax;
     public float Armor;
 
     public UnityEvent EventDeath;
+
+    [SerializeField] Slider _hpBar;
+    [SerializeField] TextMeshProUGUI _hpText;
+
+    void Awake() { 
+        Heal();
+    }
 
     public void TakeDamage(float amount) {
         if(_hp - amount > 0) {
@@ -20,15 +29,23 @@ public class RB_Health : MonoBehaviour {
     }
 
     public void Heal(float amount) {
-        if(_hp + amount < _hpMax) {
+        if(_hp + amount > _hpMax) {
             _hp += amount;
         }
-        else { 
-            _hp = _hpMax;
+        else {
+            Heal();
         }
     }
     
     public void Heal() {
         _hp = _hpMax;
+    }
+
+    void Update() {
+        if (_hp > _hpMax) {
+            Heal();
+        }
+        _hpBar.value = _hp / _hpMax;
+        _hpText.text = _hp.ToString()+" / "+_hpMax.ToString();
     }
 }

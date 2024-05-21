eab945eba8087e8e61ca8b66e88ac960900afa4d
Keuvain <118433891+Sarulpabi1@users.noreply.github.com>
2024-05-21 14:05:33 +0200

    LevelManager Phase 1 and Boss

--- Assets/Scripts/Managers/RB_LevelManager.cs
+++ Assets/Scripts/Managers/RB_LevelManager.cs
@@ -1,11 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RB_LevelManager : MonoBehaviour
 {
     public PHASES Phase;
+    public SCENENAMES CurrentScene;
     public static RB_LevelManager Instance;
+
+    public static event Action OnGameStarted;
+    public static event Action OnGamePhase1Ended;
 
     private void Awake()
     {
@@ -18,19 +24,34 @@ public class RB_LevelManager : MonoBehaviour
             return;
         }
     }
+
+    private void Start()
+    {
+        OnGameStarted?.Invoke();
+    }
     public void SwitchPhase()
     {
-        
+        if (CurrentScene != SCENENAMES.Boss1 && CurrentScene != SCENENAMES.Boss2 && CurrentScene != SCENENAMES.Boss3)
+        {
+            Phase = PHASES.Infiltration;
+        }
+        else
+        {
+            Phase = PHASES.Boss;
+        }
     }
     
     public void SpawnCombatEnemies()
     {
-        
+        if (Phase == PHASES.Combat)
+        {
+            //Spawn combat ennemies
+        }
     }
 
     public void PlayerLost() 
     {
-
+        
     }
 
     public void PlayerWon()

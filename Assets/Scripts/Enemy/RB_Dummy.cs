0c485ea6c2df4c25ed6c1bff9f7d42eaab1b0070
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-05-22 14:38:55 +0200

    Added feedback on damage, heal and death
    Did particle on damage, implemented heal and death but haven't done the particles

--- Assets/Scripts/Enemy/RB_Dummy.cs
+++ Assets/Scripts/Enemy/RB_Dummy.cs
@@ -3,11 +3,11 @@ using UnityEngine.Events;
 
 public class RB_Dummy : RB_Enemy
 {
-    [SerializeField] private GameObject DeathSpawnPrefab;
+    [SerializeField] private string DeathSpawnPrefabName;
     protected override void Death()
     {
         EventDead?.Invoke();
-        Instantiate(DeathSpawnPrefab, transform.position, transform.rotation);
+        Instantiate(Resources.Load<GameObject>("Prefabs/Enemies/" + DeathSpawnPrefabName), new Vector3(Random.Range(-3, 3), transform.position.y, Random.Range(-3, 3)), transform.rotation);
         Destroy(gameObject);
     }
 }

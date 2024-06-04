458acd645d85a71b90b3eb6f60f1c2ebd2b357bd
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-06-04 17:31:11 +0200

    Added target system on the AI BT
    -added pawn

--- Assets/Scripts/Room/RB_RoomManager.cs
+++ Assets/Scripts/Room/RB_RoomManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -65,11 +66,20 @@ public class RB_RoomManager:MonoBehaviour
 
     public List<RB_Health> GetDetectedEnemies(int roomIndex)
     {
+
+        foreach (RB_Health enemy in _rooms[roomIndex].DetectedEnemies.ToList()) 
+        {
+            if (enemy == null) _rooms[roomIndex].DetectedEnemies.Remove(enemy);
+        }
         return _rooms[roomIndex].DetectedEnemies;
     }
 
     public List<RB_Health> GetDetectedAllies(int roomIndex)
     {
+        foreach (RB_Health enemy in _rooms[roomIndex].DetectedAllies.ToList())
+        {
+            if (enemy == null) _rooms[roomIndex].DetectedAllies.Remove(enemy);
+        }
         return _rooms[roomIndex].DetectedAllies;
     }
 

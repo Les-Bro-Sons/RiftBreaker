1a35db96dc2156031cc745c7c93f3956665958e5
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-06-17 00:11:54 +0200

    Added collision on yog tentacle + sounds scale with timescale

---
+++ Assets/Scripts/Audio/RB_AudioSource.cs
@@ -0,0 +1,50 @@
+using MANAGERS;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RB_AudioSource : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    private float _basePitch;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _basePitch = _audioSource.pitch;
+
+        if (_audioSource.clip == null)
+            Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        if (RB_TimeManager.Instance.IsRewinding) //scale the pitch on rewind and timescale
+        {
+            _audioSource.pitch = Mathf.Lerp(_audioSource.pitch, -_basePitch * Time.timeScale, 4 * Time.deltaTime);
+        }
+        else
+        {
+            _audioSource.pitch = Mathf.Lerp(_audioSource.pitch, _basePitch * Time.timeScale, 4 * Time.deltaTime);
+        }
+        
+        if (!_audioSource.isPlaying)
+        {
+            if (!_audioSource.loop)
+            {
+                Destroy(gameObject);
+            }
+            else if (_audioSource.pitch < 0)
+            {
+                _audioSource.time = _audioSource.clip.length - 0.01f;
+                _audioSource.Play();
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        RB_AudioManager.Instance.AudioSources.Remove(_audioSource);
+    }
+}

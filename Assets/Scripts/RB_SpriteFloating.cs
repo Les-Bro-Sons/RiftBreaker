752f851f1116746c230ab3ec82c94e906d797104
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-06-22 14:07:15 +0200

    Added alpha to dialog box & robert floating

---
+++ Assets/Scripts/RB_SpriteFloating.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class RB_SpriteFloating : MonoBehaviour
+{
+    private Transform _transform;
+    private float _baseY;
+
+    [SerializeField] private float _floatHeight = 1;
+    [SerializeField] private float _floatSpeed = 1;
+    [SerializeField] private bool _floatAbs = false;
+
+    private void Awake()
+    {
+        _transform = transform;
+        _baseY = _transform.localPosition.y;
+    }
+
+    private void Update()
+    {
+        if (_floatAbs)
+        {
+            _transform.localPosition = (Vector3.up * Mathf.Abs(Mathf.Sin(Time.time * _floatSpeed) * _floatHeight)) + (Vector3.up * _baseY);
+        }
+        else
+        {
+            _transform.localPosition = (Vector3.up * Mathf.Sin(Time.time * _floatSpeed) * _floatHeight) + (Vector3.up * _baseY);
+        }
+    }
+}

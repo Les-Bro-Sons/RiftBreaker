625394f4612470da57a0cef9e82c6512af87185e
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-06-26 01:39:44 +0200

    Removed useless using

--- Assets/Scripts/Player/RB_Items.cs
+++ Assets/Scripts/Player/RB_Items.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using static UnityEditor.Progress;
 
 public class RB_Items : MonoBehaviour
 {

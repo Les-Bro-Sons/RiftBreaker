befe8e8b327b1fafa3820e4971a644695a5dad33
Keuvain <118433891+Sarulpabi1@users.noreply.github.com>
2024-06-26 14:47:09 +0200

    Cinematic Sounds + music + fix some sounds

--- Assets/Scripts/Cinematique/RB_EndCreditCinematic.cs
+++ Assets/Scripts/Cinematique/RB_EndCreditCinematic.cs
@@ -1,3 +1,5 @@
+using MANAGERS;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,6 +8,10 @@ public class RB_EndCreditCinematic : MonoBehaviour
     [SerializeField] private RectTransform _creditTransform;
     [SerializeField] private float _creditSpeed;
 
+    private void Start()
+    {
+        RB_AudioManager.Instance.PlayMusic("Credit_Music");
+    }
     private void Update()
     {
         _creditTransform.position += Vector3.up * _creditSpeed * Time.deltaTime;

29116e4c46b020674f07352f43b2ef020ebe44a1
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-06-26 11:10:57 +0200

    Fix door opening when robert do the clone attack

--- Assets/Scripts/Enemy/Boss/RobertLenec/RB_RobertLenec.cs
+++ Assets/Scripts/Enemy/Boss/RobertLenec/RB_RobertLenec.cs
@@ -48,6 +48,7 @@ public class RB_RobertLenec : RB_Boss
     [Header("Clone Attack (attack3)")]
     public GameObject Clone;
     [SerializeField] private List<Transform> _waypoints;
+    [SerializeField] private Transform _tpPoint;
     [SerializeField] private int _numberOfArrow;
     [SerializeField] private float _cloneAttackInterval = 0.5f;
     [SerializeField] private float _cloneAttackDelay = 1f;
@@ -307,7 +308,7 @@ public class RB_RobertLenec : RB_Boss
         //get the position of the boss before the attack
         _lastPosition = transform.position;
         //tp the boss out of the map
-        transform.position = new Vector3(transform.position.x + 3000, transform.position.y, transform.position.z);
+        transform.position = _tpPoint.position;
 
         //Cooldown
         _currentCooldownAttack3 = Random.Range(_minCooldownForAttack, _maxCooldownForAttack);

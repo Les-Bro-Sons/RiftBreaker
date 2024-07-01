036875b2bb439db94b0969cea54c1ac719ef3d37
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-07-01 04:21:47 +0200

    Applying shader to all entity correctly

--- Assets/Shader/CustomFunction/EntityPositionHoles.hlsl
+++ Assets/Shader/CustomFunction/EntityPositionHoles.hlsl
@@ -1,35 +1,40 @@
 // EntityPositionHoles.hlsl
 
-void CalculateHoles_float(float3 worldPosition, float3 cameraPosition, Texture2D entityPositionsTex, SamplerState samplerState, float posDivider, float holeRadius, out float hole)
+#define MAX_TEX_DIM 64
+
+void CalculateHoles_float(float texWidth, float texHeight, float3 worldPosition, float3 cameraPosition, Texture2D entityPositionsTex, SamplerState samplerState, float posDivider, float holeRadius, out float3 hole)
 {
     float3 entityPosition;
     float2 uv;
     float dist;
-    hole = 0.0;
-
-    // Assuming the texture is 4x4 for this example
-    float texWidth = 4.0;
-    float texHeight = 4.0;
+    //hole = 0.0;
+    hole = float3(9999, 9999, 9999);
     
-    for (int x = 0; x < 4; x++)
+    int maxTexWidth = (int) min(texWidth, MAX_TEX_DIM);
+    int maxTexHeight = (int) min(texHeight, MAX_TEX_DIM);
+    
+    for (int x = 0; x < MAX_TEX_DIM; x++)
     {
-        for (int y = 0; y < 4; y++)
+        if (x >= maxTexWidth) break;
+        for (int y = 0; y < MAX_TEX_DIM; y++)
         {
+            if (y >= maxTexHeight) break;
             uv = float2(x / texWidth, y / texHeight);
             entityPosition = entityPositionsTex.Sample(samplerState, uv).xyz * posDivider;
             dist = distance(worldPosition, entityPosition);
 
-            if (dist < holeRadius)
+            if (dist < holeRadius * 4)
             {
                 float3 flatEntityPosition = float3(entityPosition.x, 0, entityPosition.z);
                 float3 flatWorldPosition = float3(worldPosition.x, 0, worldPosition.z);
                 if (distance(flatWorldPosition, cameraPosition) < distance(cameraPosition, flatEntityPosition))
                 {
-                    hole = 1.0;
+                    //hole = 1.0;
+                    hole = entityPosition;
                 }
                 else
                 {
-                    hole = 0;
+                    //hole = 0;
                 }
                 break;
             }

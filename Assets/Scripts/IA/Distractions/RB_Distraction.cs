4c29e1f6dd1a3031742a2769c243349d0e9bdaae
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-07-04 21:30:26 +0200

    Adding rigidbody to distractions

--- Assets/Scripts/IA/Distractions/RB_Distraction.cs
+++ Assets/Scripts/IA/Distractions/RB_Distraction.cs
@@ -9,21 +9,34 @@ public class RB_Distraction : MonoBehaviour
     public float Priority = 0;
     public float SoundRadius = 0;
 
+    /// <summary>
+    /// Instantiate a new distraction
+    /// </summary>
     public static RB_Distraction NewDistraction(DISTRACTIONTYPE distractionType, Vector3 position, float priority, float soundRadius)
     {
         GameObject distractionObject = new GameObject(distractionType.ToString() + " distraction");
         RB_Distraction distraction = distractionObject.AddComponent<RB_Distraction>();
+        SphereCollider distractionCollider = distractionObject.AddComponent<SphereCollider>();
+        Rigidbody distractionRb = distractionObject.AddComponent<Rigidbody>();
+        distractionRb.useGravity = false;
+        distractionRb.excludeLayers = ~12;
+        distractionObject.layer = 12;
+
         distraction.transform = distraction.GetComponent<Transform>();
         distraction.transform.position = position;
         distraction.DistractionType = distractionType;
         distraction.Priority = priority;
         distraction.SoundRadius = soundRadius;
-        distraction.OnDistractionSpawned();
 
         return distraction;
     }
 
-    public void OnDistractionSpawned()
+    private void Start()
+    {
+        OnDistractionSpawned();
+    }
+
+    private void OnDistractionSpawned()
     {
         foreach (Collider collider in Physics.OverlapSphere(transform.position, SoundRadius, ((1 << 9) | (1 << 6))))
         {
